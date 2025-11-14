4c80b9fc6e91f36a9d22641d99fdb7dcac11dad4
Tiagotj7 <tiagowork07@gmail.com>
2025-11-14 02:32:44 -0300

    redirect url

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@ define('DB_USER', 'root');
 define('DB_PASS', ''); // no XAMPP, normalmente vazio
 
 // URL base (ajuste se necessário)
-define('BASE_URL', '/senai-chamados');
+define('BASE_URL', '/');
 
 // Uploads
 define('UPLOAD_DIR', __DIR__ . '/../uploads');
