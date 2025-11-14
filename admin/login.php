1e48963df8214a3cca12cdb115336444cf968774
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2025-11-14 00:32:45 -0300

    up login.php

---
+++ admin/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Service Manager</title>
+</head>
+<body>
+    <main>
+       
+        <form action="" method="post">
+            <h1>Entre Aqui</h1>
+            <label for="email">Email</label>
+            <input type="email" name="Email" id="email" placeholder="Digite seu email">
+            <label for="password">Senha</label>
+            <input type="text" name="Password" id="password" placeholder="Digite sua senha">
+        </form>
+    </main>
+</body>
+</html>
